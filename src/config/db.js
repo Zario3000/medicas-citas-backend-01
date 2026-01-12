efb0d6628de5933aeb087cb2a9680b0438841972
Zario3000 <nazarioortizcontreras51@gmail.com>
2026-01-12 01:53:54 -0500

    fix postgres connection using DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-pool.on('connect', async (client) => {
-  await client.query('SET search_path TO public');
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+
