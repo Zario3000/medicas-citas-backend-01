3cf07c150cd3ef45d10e7f6448406bee7cc22fdf
Zario3000 <nazarioortizcontreras51@gmail.com>
2026-01-12 01:11:36 -0500

    fix db schema and connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,11 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
+});
+
+pool.on('connect', async (client) => {
+  await client.query('SET search_path TO public');
 });
 
 module.exports = pool;
